0647c45335059ccd17b9b26f197d1e33d44c8bf1
Aric Huang <arichuang@gmail.com>
2021-06-08 18:08:51 -0700

    reformat, add process_segment stub

--- patch_en.py
+++ patch_en.py
@@ -8,6 +8,11 @@ def main():
 
     patch_file_en("b0200000.mpt")
 
+# Process a single "segment" of dialogue.
+# The resulting segment should be the exact same length as the original segment.
+def process_segment(segment):
+    return segment
+
 def patch_file_en(filename):
     print(f'Patching file en/{filename}')
     with open(f'en/{filename}', "rb") as in_file, open (f'out/{filename}', "wb") as out_file:
@@ -19,49 +24,47 @@ def patch_file_en(filename):
         final_data = bytearray("", 'utf-8')
 
         pointer = 0
-        segmentStart = None
-        segmentEnd = None
+        segment_start = None
+        segment_end = None
         while pointer < size:
-            if segmentStart == None:
+            if segment_start == None:
                 # Need to look for a new segment start.
                 if data[pointer:pointer+4] == b'@a@b':
                     # Write the segment start marker
                     final_data.extend(b'@a@b')
 
-                    segmentStart = pointer+4
+                    segment_start = pointer+4
                 else:
                     # Write any bytes encountered between segments to the output buffer
                     final_data.append(data[pointer])
 
                     pointer += 1
-            elif segmentEnd == None:
+            elif segment_end == None:
                 # Need to look for a new segment end.
                 if data[pointer:pointer+4] == b'@c2@' or data[pointer:pointer+4] == b'@c0@':
-                    segmentEnd = pointer
+                    segment_end = pointer
                     pointer = pointer+4
                 else:
                     pointer += 1
             else:
                 # We have a start and end to the segment.
-                segment = data[segmentStart:segmentEnd]
+                segment = data[segment_start:segment_end]
                 segmentSize = len(segment)
 
                 print(f'Processing segment ({segmentSize} bytes): {segment}')
 
                 # Process the segment.
-                processedSegment = bytearray("", 'utf-8')
+                processedSegment = process_segment(segment)
                 
-
                 # Write the processed segment.
-                processedSegment = segment[0:]
                 final_data.extend(processedSegment)
 
                 # Write the segment end marker
-                final_data.extend(data[segmentEnd:segmentEnd+4])
+                final_data.extend(data[segment_end:segment_end+4])
 
                 # Reset the segment start/end pointers.
-                segmentStart = None
-                segmentEnd = None
+                segment_start = None
+                segment_end = None
 
                 # break
 
